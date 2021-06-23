a409b4a7dab053573e710e6f7bdbd13badfc7a30
AvirathPaliwal <73025697+AvirathPaliwal@users.noreply.github.com>
2021-06-23 18:38:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import Ionicons from 'react-native-vector-icons/Ionicons';
+// import Feed from './screens/Feed';
+// import CreateStory from './screens/CreateStory';
+import DrawerNavigator from './navigation/DrawerNavigator';
+
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+     <DrawerNavigator/>
+    </NavigationContainer>
+  );
+}
+
